28e0e55fd5f6d4188463f600b4d5c8291ff1953b
Zachary Whitley <mail@zacharywhitley.com>
2016-06-01 11:11:21 -0400

    Add functions for converting inet address to and from long

---
+++ src/main/java/com/semantalytics/stardog/function/util/InetAddressToNumber.java
@@ -0,0 +1,61 @@
+package com.semantalytics.stardog.function.util;
+
+
+import com.complexible.common.rdf.model.Values;
+import com.complexible.stardog.plan.filter.ExpressionEvaluationException;
+import com.complexible.stardog.plan.filter.ExpressionVisitor;
+import com.complexible.stardog.plan.filter.functions.AbstractFunction;
+import com.complexible.stardog.plan.filter.functions.Function;
+import com.complexible.stardog.plan.filter.functions.UserDefinedFunction;
+import com.google.common.base.Splitter;
+import org.openrdf.model.Value;
+
+import java.util.List;
+
+/**
+ * Given the dotted-quad representation of an IPv4 network address as a string, returns an
+ * integer that represents the numeric value of the address in network byte order (big endian)
+ */
+public class InetAddressToNumber extends AbstractFunction implements UserDefinedFunction {
+
+    private static final long FIRST_OCTET_BASE = 16777216;
+    private static final long SECOND_OCTET_BASE = 65536;
+    private static final long THIRD_OCTET_BASE = 256;
+    private static final long FOURTH_OCTET_BASE = 1;
+
+    public InetAddressToNumber() {
+        super(1, "http://semantalytics.com/2016/03/ns/stardog/udf/util/inetToNum");
+    }
+
+    private InetAddressToNumber(InetAddressToNumber inetAddressToNumber) {
+        super(inetAddressToNumber);
+    }
+
+    @Override
+    protected Value internalEvaluate(final Value... values) throws ExpressionEvaluationException {
+
+        assertStringLiteral(values[0]);
+
+        List<String> ipQuads = Splitter.on('.').splitToList(values[0].toString());
+
+        if(ipQuads.size() != 4) {
+            throw new ExpressionEvaluationException("Argument must be fill ip quad. Found " + values[0].stringValue());
+        }
+
+        return Values.literal(FIRST_OCTET_BASE * Long.valueOf(ipQuads.get(0)) +
+                              SECOND_OCTET_BASE * Long.valueOf(ipQuads.get(1)) +
+                              THIRD_OCTET_BASE * Long.valueOf(ipQuads.get(2)) +
+                              FOURTH_OCTET_BASE * Long.valueOf(ipQuads.get(3)));
+    }
+
+    @Override
+    public Function copy() {
+        return new InetAddressToNumber(this);
+    }
+
+    @Override
+    public void accept(final ExpressionVisitor expressionVisitor) {
+        expressionVisitor.visit(this);
+    }
+
+}
