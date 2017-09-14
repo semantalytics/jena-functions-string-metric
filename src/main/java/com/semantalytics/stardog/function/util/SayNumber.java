ee53beff38f109005bb73fbb1d079c3e6356c9b6
Zachary Whitley <mail@zacharywhitley.com>
2017-09-14 09:46:44 -0400

    Update SayNumber.java

--- src/main/java/com/semantalytics/stardog/function/util/SayNumber.java
+++ src/main/java/com/semantalytics/stardog/function/util/SayNumber.java
@@ -1,1 +1,53 @@
 
+package com.semantalytics.stardog.function.util;
+
+import com.complexible.common.rdf.model.Values;
+import com.complexible.stardog.plan.filter.ExpressionEvaluationException;
+import com.complexible.stardog.plan.filter.ExpressionVisitor;
+import com.complexible.stardog.plan.filter.functions.AbstractFunction;
+import com.complexible.stardog.plan.filter.functions.UserDefinedFunction;
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
+import org.openrdf.model.Value;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class SayNumber extends AbstractFunction implements UserDefinedFunction {
+
+    public SayNumber() {
+        super(1, "http://semantalytics.com/2016/03/ns/stardog/udf/util/sayNumber");
+    }
+
+    private SayNumber(final SayNumber sayNumber) {
+        super(sayNumber);
+    }
+
+    @Override
+    protected Value internalEvaluate(Value... values) throws ExpressionEvaluationException {
+        int value = assertIntegerLiteral(values[0]).intValue();
+        
+        RuleBasedNumberFormat nf = new RuleBasedNumberFormat(Locale.US, RuleBasedNumberFormat.SPELLOUT);
+
+        ValueConverter converter = ValueConverters.ENGLISH_INTEGER;
+        String valueAsWords = converter.asWords(value);
+
+        return Values.literal(valueAsWords);
+    }
+
+    @Override
+    public SayNumber copy() {
+        return new SayNumber(this);
+    }
+
+    @Override
+    public void accept(final ExpressionVisitor expressionVisitor) {
+        expressionVisitor.visit(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Convert integer into spoken equivalent";
+    }
+}
