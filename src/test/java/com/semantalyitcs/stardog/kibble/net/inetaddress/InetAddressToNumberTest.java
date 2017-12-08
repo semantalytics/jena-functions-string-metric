b93ea1b47f7ff700356f86e89c8a72a07da33d4b
Zachary <zachary.whitley@gmail.com>
2017-12-07 20:38:08 -0500

    Consolidate to single pom

---
+++ src/test/java/com/semantalyitcs/stardog/kibble/net/inetaddress/InetAddressToNumberTest.java
@@ -0,0 +1,89 @@
+package com.semantalytics.stardog.kibble.net.inetaddress;
+
+import com.complexible.stardog.Stardog;
+import com.complexible.stardog.api.Connection;
+import com.complexible.stardog.api.ConnectionConfiguration;
+import com.complexible.stardog.api.admin.AdminConnection;
+import com.complexible.stardog.api.admin.AdminConnectionConfiguration;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openrdf.query.TupleQueryResult;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class InetAddressToNumberTest {
+
+    protected static Stardog SERVER = null;
+    protected static final String DB = "test";
+    private Connection aConn;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        SERVER = Stardog.builder().create();
+
+        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
+                .credentials("admin", "admin")
+                .connect();
+
+        try {
+            if (aConn.list().contains(DB)) {
+                aConn.drop(DB);
+            }
+
+            aConn.newDatabase(DB).create();
+        }
+        finally {
+            aConn.close();
+        }
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        if (SERVER != null) {
+            SERVER.shutdown();
+        }
+    }
+
+    @Before
+    public void setUp() {
+        aConn = ConnectionConfiguration.to(DB)
+                .credentials("admin", "admin")
+                .connect();
+    }
+
+    @After
+    public void tearDown() {
+        aConn.close();
+    }
+
+    @Test
+    public void testInetAddressToNumber() throws Exception {
+        final Connection aConn = ConnectionConfiguration.to(DB)
+                .credentials("admin", "admin")
+                .connect();
+
+            aConn.begin();
+
+            final String aQuery = "prefix util: <" + InetAddressVocabulary.NAMESPACE + "> " +
+                    "select ?result where { bind(util:inetAddressToNumber(\"192.168.0.1\") as ?result) }";
+
+            try (final TupleQueryResult aResult = aConn.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final long aValue = Long.parseLong(aResult.next().getValue("result").stringValue());
+
+                assertEquals(3232235521L, aValue);
+
+                assertFalse("Should have no more results", aResult.hasNext());
+            } finally {
+                aConn.close();
+            }
+    }
+}
---
+++ src/test/java/com/semantalyitcs/stardog/kibble/net/internetdomainname/InternetAddressTest.java
@@ -0,0 +1,99 @@
+package com.semantalytics.stardog.kibble.net;
+
+import com.complexible.stardog.Stardog;
+import com.complexible.stardog.api.Connection;
+import com.complexible.stardog.api.ConnectionConfiguration;
+import com.complexible.stardog.api.admin.AdminConnection;
+import com.complexible.stardog.api.admin.AdminConnectionConfiguration;
+import com.complexible.stardog.plan.eval.ExecutionException;
+import org.junit.*;
+import org.openrdf.query.TupleQueryResult;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+
+public class InternetAddressTest {
+
+    protected static Stardog SERVER = null;
+    protected static final String DB = "test";
+    private Connection aConn;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        SERVER = Stardog.builder().create();
+
+        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
+                .credentials("admin", "admin")
+                .connect();
+
+        try {
+            if (aConn.list().contains(DB)) {
+                aConn.drop(DB);
+            }
+
+            aConn.newDatabase(DB).create();
+        }
+        finally {
+            aConn.close();
+        }
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        if (SERVER != null) {
+            SERVER.shutdown();
+        }
+    }
+
+    @Before
+    public void setUp() {
+        aConn = ConnectionConfiguration.to(DB)
+                .credentials("admin", "admin")
+                .connect();
+    }
+
+    @After
+    public void tearDown() {
+        aConn.close();
+    }
+
+    @Ignore
+    @Test
+    public void testBindPrev() throws Exception {
+
+        try(final Connection aConn = ConnectionConfiguration.to(DB)
+                                                            .credentials("admin", "admin")
+                                                            .connect()) {
+
+            aConn.begin();
+
+            final String aQuery = "prefix util: <" + InternetDomainNameVocabulary.NAMESPACE + "> " +
+                    "select ?result where { bind(util:bindPrev(?v) as ?result) values ?v {1 2 3 4 5} } order by ?v";
+
+
+            List<String> results = new ArrayList(5);
+            List<String> expected = Arrays.asList(null, "1", "2", "3", "4");
+
+            try(final TupleQueryResult aResult = aConn.select(aQuery).execute()) {
+
+                //final String aValue = aResult.next().getValue("result").stringValue();
+                while(aResult.hasNext()) {
+                    results.add(aResult.next().getValue("result").stringValue());
+                }
+            }
+
+            assertEquals(expected, results);
+
+            aConn.close();
+
+        } catch (final ExecutionException e) {
+            aConn.rollback();
+        }
+    }
+
+}
