212c928a345f8c3b208adf2504b1cf09267d682c
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 15:35:28 -0500

    Update EmojifyTest.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/emoji/EmojifyTest.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/emoji/EmojifyTest.java
@@ -12,55 +12,14 @@ import org.openrdf.query.TupleQueryResult;
 import static org.junit.Assert.*;
 
 
-public class EmojifyTest {
+public class EmojifyTest  extends AbstractStardogTest {
 
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    private Connection aConn;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        aConn.close();
-    }
+  
 
     @Test
     public void testEmojify() throws Exception {
 
-            aConn.begin();
+     
 
             final String aQuery = "prefix emoji: <http://semantalytics.com/2017/11/ns/stardog/strings/emoji/>" +
                     "select ?result where { bind(emoji:emojify(\"A :cat:, :dog: and a :mouse: became friends<3. For :dog:'s birthday party, they all had :hamburger:s, :fries:s, :cookie:s and :cake:.\") as ?result) }";
@@ -84,7 +43,7 @@ public class EmojifyTest {
                 "select ?result where { bind(emoji:isEmoji() as ?result) }";
 
         final TupleQueryResult aResult = aConn.select(aQuery).execute();
-        try {
+    
             // there should be a result because implicit in the query is the singleton set, so because the bind
             // should fail due to the value error, we expect a single empty binding
             assertTrue("Should have a result", aResult.hasNext());
@@ -94,9 +53,7 @@ public class EmojifyTest {
             assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
             assertFalse("Should have no more results", aResult.hasNext());
-        } finally {
-            aResult.close();
-        }
+   
     }
 
     @Test
@@ -106,7 +63,7 @@ public class EmojifyTest {
                 "select ?result where { bind(emoji:isEmoji(\"star\", \"dog\") as ?result) }";
 
         final TupleQueryResult aResult = aConn.select(aQuery).execute();
-        try {
+   
             // there should be a result because implicit in the query is the singleton set, so because the bind
             // should fail due to the value error, we expect a single empty binding
             assertTrue("Should have a result", aResult.hasNext());
@@ -116,9 +73,7 @@ public class EmojifyTest {
             assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
             assertFalse("Should have no more results", aResult.hasNext());
-        } finally {
-            aResult.close();
-        }
+       
     }
 
     @Test
@@ -128,7 +83,7 @@ public class EmojifyTest {
                 "select ?result where { bind(emoji:isEmoji(<http://example.com>) as ?result) }";
 
         final TupleQueryResult aResult = aConn.select(aQuery).execute();
-        try {
+    
             // there should be a result because implicit in the query is the singleton set, so because the bind
             // should fail due to the value error, we expect a single empty binding
             assertTrue("Should have a result", aResult.hasNext());
@@ -138,9 +93,7 @@ public class EmojifyTest {
             assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
             assertFalse("Should have no more results", aResult.hasNext());
-        } finally {
-            aResult.close();
-        }
+      
     }
 
     @Test
@@ -150,7 +103,7 @@ public class EmojifyTest {
                 "select ?result where { bind(emoji:isEmoji(1) as ?result) }";
 
         final TupleQueryResult aResult = aConn.select(aQuery).execute();
-        try {
+     
             // there should be a result because implicit in the query is the singleton set, so because the bind
             // should fail due to the value error, we expect a single empty binding
             assertTrue("Should have a result", aResult.hasNext());
@@ -160,9 +113,7 @@ public class EmojifyTest {
             assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
             assertFalse("Should have no more results", aResult.hasNext());
-        } finally {
-            aResult.close();
-        }
+      
     }
 
 }
