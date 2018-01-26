59a1dd1e6928b716dfd19aeb29e9c1c7891b179e
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 15:37:35 -0500

    Update TestCsv.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/escape/TestCsv.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/escape/TestCsv.java
@@ -13,55 +13,13 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
-public class TestCsv {
+public class TestCsv  extends AbstractStardogTest {
 
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    private Connection aConn;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        aConn.close();
-    }
+   
 
     @Test
     public void testEpochTime() throws Exception {
 
-            aConn.begin();
 
             final String aQuery = "prefix escape: <" + EscapeVocabulary.NAMESPACE + ">" +
                     "select ?result where { bind(escape:csv(\"2017-09-01\") as ?result) }";
