a8ec8f2f74571fe634b7f70c6e31c2e6fc1dac04
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 11:36:10 -0500

    Update IsNoneBlank.java

--- src/main/java/com/semantalytics/stardog/kibble/strings/string/IsNoneBlank.java
+++ src/main/java/com/semantalytics/stardog/kibble/strings/string/IsNoneBlank.java
@@ -12,7 +12,7 @@ import static com.complexible.common.rdf.model.Values.*;
 public final class IsNoneBlank extends AbstractFunction implements StringFunction {
 
     protected IsNoneBlank() {
-        super(1, StringVocabulary.isNoneBlank.toString());
+        super(1, StringVocabulary.isNoneBlank.stringValue());
     }
 
     private IsNoneBlank(final IsNoneBlank isNoneBlank) {
