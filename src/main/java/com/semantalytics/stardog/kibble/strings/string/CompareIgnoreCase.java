226e466f24db203f97423c6bab2f42e2c79317f1
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 11:28:13 -0500

    Update CompareIgnoreCase.java

--- src/main/java/com/semantalytics/stardog/kibble/strings/string/CompareIgnoreCase.java
+++ src/main/java/com/semantalytics/stardog/kibble/strings/string/CompareIgnoreCase.java
@@ -11,7 +11,7 @@ import org.openrdf.model.Value;
 public final class CompareIgnoreCase extends AbstractFunction implements StringFunction {
 
     protected CompareIgnoreCase() {
-        super(2, StringVocabulary.compareIgnoreCase.toString());
+        super(2, StringVocabulary.compareIgnoreCase.stringValue());
     }
 
     private CompareIgnoreCase(final CompareIgnoreCase compareIgnoreCase) {
