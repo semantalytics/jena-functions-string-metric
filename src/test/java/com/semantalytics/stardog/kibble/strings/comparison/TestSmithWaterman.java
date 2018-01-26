e6d32caa51bc1790642d2e06073ec210a97c177e
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 15:41:34 -0500

    Update TestSmithWaterman.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/comparison/TestSmithWaterman.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/comparison/TestSmithWaterman.java
@@ -11,64 +11,21 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class TestSmithWaterman {
+public class TestSmithWaterman  extends AbstractStardogTest {
 
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    Connection connection;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        connection = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        connection.close();
-    }
-
-    private Connection aConn;
+  
 
     @Test
     public void testSmithWatermanTwoArg() throws Exception {
 
-        try {
+    
 
             final String aQuery = "prefix ss: <" + StringComparisonVocabulary.NAMESPACE + "> " +
                     "select ?result where { bind(ss:smithWaterman(\"Stardog\", \"Starman\") as ?result) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
 
-            try {
+     
                 assertTrue("Should have a result", aResult.hasNext());
 
                 final String aValue = aResult.next().getValue("result").stringValue();
@@ -76,27 +33,20 @@ public class TestSmithWaterman {
                 assertEquals(0.5714286, Double.parseDouble(aValue), 0.00001);
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+       
     }
 
     @Test
     public void testSmithWatermanSevenArg() throws Exception {
 
-        try {
+    
 
             final String aQuery = "prefix ss: <" + StringComparisonVocabulary.NAMESPACE + "> " +
                     "select ?result where { bind(ss:smithWaterman(\"Stardog\", \"Starman\", -5.0, -1.0, 5.0, -3.0, " + Integer.MAX_VALUE + ") as ?result) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
 
-            try {
+        
                 assertTrue("Should have a result", aResult.hasNext());
 
                 final String aValue = aResult.next().getValue("result").stringValue();
@@ -104,26 +54,19 @@ public class TestSmithWaterman {
                 assertEquals(0.5714286, Double.parseDouble(aValue), 0.00001);
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+          
     }
 
     @Test
     public void testSmithWatermanWrongNumberOfArgs3() throws Exception {
 
-        try {
+    
 
             final String aQuery = "prefix ss: <" + StringComparisonVocabulary.NAMESPACE + "> " +
                     "select ?result where { bind(ss:smithWaterman(\"Stardog\", \"Starman\", 1.0) as ?result) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+       
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -133,26 +76,19 @@ public class TestSmithWaterman {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+           
     }
 
     @Test
     public void testSmithWatermanWrongType() throws Exception {
 
-        try {
+      
 
             final String aQuery = "prefix ss: <" + StringComparisonVocabulary.NAMESPACE + "> " +
                     "select ?result where { bind(ss:smithWaterman(7) as ?result) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+       
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -162,13 +98,6 @@ public class TestSmithWaterman {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+           
     }
 }
