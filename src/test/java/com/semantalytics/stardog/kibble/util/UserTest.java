ec245d8ed4e5a2b15f41bcc42e4a2b426599388c
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 15:42:53 -0500

    Update UserTest.java

--- src/test/java/com/semantalytics/stardog/kibble/util/UserTest.java
+++ src/test/java/com/semantalytics/stardog/kibble/util/UserTest.java
@@ -11,57 +11,15 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class UserTest {
+public class UserTest  extends AbstractStardogTest {
 
 
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    private Connection aConn;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        aConn.close();
-    }
+   
 
     @Test
     public void testQuarter() throws Exception {
 
-            aConn.begin();
-
+        
             final String aQuery = "prefix date: <" + UtilVocabulary.NAMESPACE + ">" +
                     "select ?result where { bind(date:user() as ?result) }";
 
@@ -75,6 +33,6 @@ public class UserTest {
 
                 assertFalse("Should have no more results", aResult.hasNext());
             }
-    }
+
 
 }
