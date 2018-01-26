9a9c4088e85d281f60d107e9437473df00f79675
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 14:39:42 -0500

    Update LowerCaseFullyTest.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/LowerCaseFullyTest.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/LowerCaseFullyTest.java
@@ -10,51 +10,8 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class LowerCaseFullyTest {
+public class LowerCaseFullyTest  extends AbstractStardogTest {
 
-
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    private Connection aConn;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        aConn.close();
-    }
 
     @Test
     public void testLowerCaseFully() throws Exception {
