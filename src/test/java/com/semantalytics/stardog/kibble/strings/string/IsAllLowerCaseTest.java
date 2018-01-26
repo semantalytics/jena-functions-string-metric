dcb86d40a9fc5fc3ef99ab4634a444391b3ccbbd
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 14:34:29 -0500

    Update IsAllLowerCaseTest.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/IsAllLowerCaseTest.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/IsAllLowerCaseTest.java
@@ -11,59 +11,12 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class IsAllLowerCaseTest {
+public class IsAllLowerCaseTest  extends AbstractStardogTest {
 
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    private Connection aConn;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        aConn.close();
-    }
-
+  
     @Test
     public void testAbbreviateMiddle() throws Exception {
-        final Connection aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-
+      
             final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
                     "select ?abbreviation where { bind(string:isAllLowerCase(\"Stardog graph database\", \"...\", 8) AS ?abbreviation) }";
 
@@ -78,26 +31,18 @@ public class IsAllLowerCaseTest {
 
                 assertFalse("Should have no more results", aResult.hasNext());
             }
-        }
-        finally {
-            aConn.close();
-        }
+       
     }
 
     @Test
     public void testEmptyString() throws Exception {
-        final Connection aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-
+    
             final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
                     "select ?abbreviation where { bind(string:isAllLowerCase(\"\", 5) as ?abbreviation) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
 
-            try {
+   
                 assertTrue("Should have a result", aResult.hasNext());
 
                 final String aValue = aResult.next().getValue("abbreviation").stringValue();
@@ -105,29 +50,17 @@ public class IsAllLowerCaseTest {
                 assertEquals("", aValue);
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+         
     }
 
     @Test
     public void testTooFewArgs() throws Exception {
 
-        final Connection aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
             final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
                     "select ?abbreviation where { bind(string:isAllLowerCase(\"one\") as ?abbreviation) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+            
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -137,30 +70,19 @@ public class IsAllLowerCaseTest {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+           
     }
 
 
     @Test
     public void testTooManyArgs() throws Exception {
 
-        final Connection aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
+       
             final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
                     "select ?abbreviation where { bind(string:isAllLowerCase(\"one\", 2, \"three\") as ?abbreviation) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+     
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -170,31 +92,20 @@ public class IsAllLowerCaseTest {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+          
     }
 
 
 
     @Test
     public void testWrongTypeFirstArg() throws Exception {
-        final Connection aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
+       
 
             final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
                     "select ?abbreviation where { bind(string:isAllLowerCase(4, 5) as ?abbreviation) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+         
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -204,29 +115,17 @@ public class IsAllLowerCaseTest {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+           
     }
 
     @Test
     public void testWrongTypeSecondArg() throws Exception {
-        final Connection aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-
+       
             final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
                     "select ?abbreviation where { bind(string:isAllLowerCase(\"one\", \"two\") as ?abbreviation) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+          
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -236,29 +135,18 @@ public class IsAllLowerCaseTest {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+           
     }
 
     @Test
     public void testLengthTooShort() throws Exception {
-        final Connection aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
+       
 
             final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
                     "select ?abbreviation where { bind(string:isAllLowerCase(\"Stardog\", 3) as ?abbreviation) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+           
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -268,13 +156,6 @@ public class IsAllLowerCaseTest {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+            
     }
 }
