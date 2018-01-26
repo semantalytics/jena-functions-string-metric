68d75a9a14e434b7e12b3974128074b3f0a676db
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 15:35:59 -0500

    Update DecimalHtmlTest.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/emoji/DecimalHtmlTest.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/emoji/DecimalHtmlTest.java
@@ -11,55 +11,13 @@ import org.openrdf.query.TupleQueryResult;
 import static org.junit.Assert.*;
 
 
-public class DecimalHtmlTest {
+public class DecimalHtmlTest  extends AbstractStardogTest {
 
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    private Connection aConn;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        aConn.close();
-    }
-
+  
     @Test
     public void testEmoji() throws Exception {
 
-            aConn.begin();
+        
 
             final String aQuery = "prefix emoji: <http://semantalytics.com/2017/11/ns/stardog/strings/emoji/>" +
                     "select ?result where { bind(emoji:emoji(\"dog\") as ?result) }";
