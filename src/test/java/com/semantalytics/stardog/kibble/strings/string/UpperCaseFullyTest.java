31c7a5817ce1a28bca1c47c33907186e5334e30f
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 14:31:48 -0500

    Update UpperCaseFullyTest.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/UpperCaseFullyTest.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/UpperCaseFullyTest.java
@@ -10,50 +10,8 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class UpperCaseFullyTest {
+public class UpperCaseFullyTest  extends AbstractStardogTest {
 
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    private Connection aConn;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        aConn.close();
-    }
 
     @Test
     public void testUpperCaseFully() throws Exception {
@@ -74,5 +32,4 @@ public class UpperCaseFullyTest {
                 assertFalse("Should have no more results", aResult.hasNext());
             }
     }
-
 }
