2d254d3a5a025aaf3b5b616d247bde6f0bbad861
Zachary Whitley <mail@zacharywhitley.com>
2018-01-26 15:38:49 -0500

    Update TestWeightedLevenshteinDistance.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/comparison/TestWeightedLevenshteinDistance.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/comparison/TestWeightedLevenshteinDistance.java
@@ -11,51 +11,9 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class TestWeightedLevenshteinDistance {
+public class TestWeightedLevenshteinDistance  extends AbstractStardogTest {
 
-    protected static Stardog SERVER = null;
-    protected static final String DB = "test";
-    private Connection aConn;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        SERVER = Stardog.builder().create();
-
-        final AdminConnection aConn = AdminConnectionConfiguration.toEmbeddedServer()
-                .credentials("admin", "admin")
-                .connect();
-
-        try {
-            if (aConn.list().contains(DB)) {
-                aConn.drop(DB);
-            }
-
-            aConn.newDatabase(DB).create();
-        }
-        finally {
-            aConn.close();
-        }
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        if (SERVER != null) {
-            SERVER.shutdown();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        aConn = ConnectionConfiguration.to(DB)
-                .credentials("admin", "admin")
-                .connect();
-    }
-
-    @After
-    public void tearDown() {
-        aConn.close();
-    }
-
+   
     @Test
     public void testLevenshtein() throws Exception {
 
@@ -79,12 +37,12 @@ public class TestWeightedLevenshteinDistance {
     public void testLevenstheinTooManyArgs() throws Exception {
 
 
-        try {
+    
             final String aQuery = "prefix ss: <" + StringComparisonVocabulary.NAMESPACE + "> " +
                     "select ?str where { bind(ss:weightedLevenshtein(\"one\", \"two\", \"three\") as ?str) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+       
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -93,26 +51,19 @@ public class TestWeightedLevenshteinDistance {
 
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+            
     }
 
     @Test
     public void testLevenshteinWrongType() throws Exception {
 
-        try {
+     
 
             final String aQuery = "prefix ss: <" + StringComparisonVocabulary.NAMESPACE + "> " +
                     "select ?str where { bind(ss:weightedLevenshtein(7) as ?str) }";
 
             final TupleQueryResult aResult = aConn.select(aQuery).execute();
-            try {
+       
                 // there should be a result because implicit in the query is the singleton set, so because the bind
                 // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
@@ -122,13 +73,6 @@ public class TestWeightedLevenshteinDistance {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            }
-            finally {
-                aResult.close();
-            }
-        }
-        finally {
-            aConn.close();
-        }
+          
     }
 }
