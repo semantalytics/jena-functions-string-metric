d07f07e2253cbdbfff1fd937874be05b876654b1
Zachary Whitley <mail@zacharywhitley.com>
2018-01-30 16:31:24 -0500

    Update Initials.java

--- src/main/java/com/semantalytics/stardog/kibble/strings/string/Initials.java
+++ src/main/java/com/semantalytics/stardog/kibble/strings/string/Initials.java
@@ -11,7 +11,7 @@ import org.openrdf.model.Value;
 public final class Initials extends AbstractFunction implements StringFunction {
 
     protected Initials() {
-        super(2, StringVocabulary.initials.stringValue());
+        super(Range.closed(1,2), StringVocabulary.initials.stringValue());
     }
 
     private Initials(final Initials initials) {
@@ -22,9 +22,14 @@ public final class Initials extends AbstractFunction implements StringFunction {
     protected Value internalEvaluate(final Value... values) throws ExpressionEvaluationException {
       
       final String string = assertStringLiteral(values[0]).stringValue();
-      final String delimiters = assertStringLiteral(values[1]).stringValue();
-
-      return Values.literal(WordUtils.initials(string, delimiters.toCharArray()));
+        
+      if(values.length = 1) {  
+          return Values.literal(WordUtils.initials(string));
+      }
+      if(values.length = 2) {
+          final String delimiters = assertStringLiteral(values[1]).stringValue();
+          return Values.literal(WordUtils.initials(string, delimiters.toCharArray()));
+      }
     }
 
     @Override
