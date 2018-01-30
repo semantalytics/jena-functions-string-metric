1b941e60c8cb49ef315ba5be65b04d151a7defb8
Zachary Whitley <mail@zacharywhitley.com>
2018-01-30 16:48:09 -0500

    Update CapitalizeTest.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/CapitalizeTest.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/CapitalizeTest.java
@@ -10,12 +10,28 @@ import static org.junit.Assert.*;
 public class CapitalizeTest  extends AbstractStardogTest {
    
     @Test
-    public void testCapitalize() throws Exception {
+    public void testCapitalize() {
      
-
-            final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
+         final String aQuery = StringVocabulary.sparqlPrefix("string") +
                     "select ?result where { bind(string:capitalize(\"stardog\") AS ?result) }";
 
+            try (final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final String aValue = aResult.next().getValue("result").stringValue();
+
+                assertEquals("Stardog", aValue);
+
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testAlreadyCapitalized() {
+   
+         final String aQuery = StringVocabulary.sparqlPrefix("string") +
+               "select ?result where { bind(string:capitalize(\"Stardog\") AS ?result) }";
 
             try (final TupleQueryResult aResult = connection.select(aQuery).execute()) {
 
@@ -27,34 +43,12 @@ public class CapitalizeTest  extends AbstractStardogTest {
 
                 assertFalse("Should have no more results", aResult.hasNext());
             }
-      
     }
 
     @Test
-    public void testAlreadyCapitalized() throws Exception {
+    public void testAllCaps() {
    
-            final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
-                    "select ?result where { bind(string:capitalize(\"Stardog\") AS ?result) }";
-
-
-            try (final TupleQueryResult aResult = connection.select(aQuery).execute()) {
-
-                assertTrue("Should have a result", aResult.hasNext());
-
-                final String aValue = aResult.next().getValue("result").stringValue();
-
-                assertEquals("Stardog", aValue);
-
-                assertFalse("Should have no more results", aResult.hasNext());
-            }
-
-      
-    }
-
-    @Test
-    public void testAllCaps() throws Exception {
-   
-            final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
+         final String aQuery = StringVocabulary.sparqlPrefix("string") +
                     "select ?result where { bind(string:capitalize(\"STARDOG\") AS ?result) }";
 
 
@@ -68,19 +62,16 @@ public class CapitalizeTest  extends AbstractStardogTest {
 
                 assertFalse("Should have no more results", aResult.hasNext());
             }
-      
     }
 
     @Test
-    public void testEmptyString() throws Exception {
+    public void testEmptyString() {
      
-
-            final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
+         final String aQuery = StringVocabulary.sparqlPrefix("string") +
                     "select ?result where { bind(string:capitalize(\"\") as ?result) }";
 
             final TupleQueryResult aResult = connection.select(aQuery).execute();
 
-       
                 assertTrue("Should have a result", aResult.hasNext());
 
                 final String aValue = aResult.next().getValue("result").stringValue();
@@ -88,20 +79,16 @@ public class CapitalizeTest  extends AbstractStardogTest {
                 assertEquals("", aValue);
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            
     }
 
     @Test
-    public void testTooFewArgs() throws Exception {
+    public void testTooFewArgs() {
 
-    
-            final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
+         final String aQuery = StringVocabulary.sparqlPrefix("string") +
                     "select ?result where { bind(string:capitalize() as ?result) }";
 
             final TupleQueryResult aResult = connection.select(aQuery).execute();
-        
-                // there should be a result because implicit in the query is the singleton set, so because the bind
-                // should fail due to the value error, we expect a single empty binding
+    
                 assertTrue("Should have a result", aResult.hasNext());
 
                 final BindingSet aBindingSet = aResult.next();
@@ -109,21 +96,16 @@ public class CapitalizeTest  extends AbstractStardogTest {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-           
     }
 
-
     @Test
-    public void testTooManyArgs() throws Exception {
+    public void testTooManyArgs() {
 
-     
-            final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
+         final String aQuery = StringVocabulary.sparqlPrefix("string") +
                     "select ?capitalize where { bind(string:capitalize(\"one\", \"two\") as ?result) }";
 
             final TupleQueryResult aResult = connection.select(aQuery).execute();
-     
-                // there should be a result because implicit in the query is the singleton set, so because the bind
-                // should fail due to the value error, we expect a single empty binding
+
                 assertTrue("Should have a result", aResult.hasNext());
 
                 final BindingSet aBindingSet = aResult.next();
@@ -131,20 +113,16 @@ public class CapitalizeTest  extends AbstractStardogTest {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-            
     }
 
     @Test
-    public void testArgumentWrongType() throws Exception {
+    public void testArgumentWrongType() {
      
-
-            final String aQuery = "prefix string: <" + StringVocabulary.NAMESPACE + "> " +
+         final String aQuery = StringVocabulary.sparqlPrefix("string") +
                     "select ?result where { bind(string:capitalize(1) as ?result) }";
 
             final TupleQueryResult aResult = connection.select(aQuery).execute();
          
-                // there should be a result because implicit in the query is the singleton set, so because the bind
-                // should fail due to the value error, we expect a single empty binding
                 assertTrue("Should have a result", aResult.hasNext());
 
                 final BindingSet aBindingSet = aResult.next();
@@ -152,6 +130,5 @@ public class CapitalizeTest  extends AbstractStardogTest {
                 assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
 
                 assertFalse("Should have no more results", aResult.hasNext());
-           
     }
 }
