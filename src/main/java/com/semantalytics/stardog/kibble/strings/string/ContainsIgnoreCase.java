b17ae5ab6630e4b25daaee576fec5455f49b685d
Zachary Whitley <mail@zacharywhitley.com>
2018-02-02 10:52:04 -0500

    Create ContainsIgnoreCase.java

---
+++ src/main/java/com/semantalytics/stardog/kibble/strings/string/ContainsIgnoreCase.java
@@ -0,0 +1,44 @@
+package com.semantalytics.stardog.kibble.strings.string;
+
+import com.complexible.common.rdf.model.Values;
+import com.complexible.stardog.plan.filter.ExpressionEvaluationException;
+import com.complexible.stardog.plan.filter.ExpressionVisitor;
+import com.complexible.stardog.plan.filter.functions.AbstractFunction;
+import com.complexible.stardog.plan.filter.functions.string.StringFunction;
+import org.apache.commons.lang3.StringUtils;
+import org.openrdf.model.Value;
+
+public final class ContainsIgnoreCase extends AbstractFunction implements StringFunction {
+
+    protected ContainsIgnoreCase() {
+        super(1, StringVocabulary.containsIgnoreCase.stringValue());
+    }
+
+    private Contains(final ContainsIgnoreCase containsIgnoreCase) {
+        super(containsIgnoreCase);
+    }
+
+    @Override
+    protected Value internalEvaluate(final Value... values) throws ExpressionEvaluationException {
+
+        final String sequence = assertStringLiteral(values[0]).stringValue();
+        final String searchSequence = assertStringLiteral(values[1]).stringValue();
+
+        return Values.literal(StringUtils.containsIgnoreCase(sequence, searchSequence));
+    }
+
+    @Override
+    public ContainsIgnoreCase copy() {
+        return new ContainsIgnoreCase(this);
+    }
+
+    @Override
+    public void accept(final ExpressionVisitor expressionVisitor) {
+        expressionVisitor.visit(this);
+    }
+
+    @Override
+    public String toString() {
+        return StringVocabulary.containsIgnoreCase.name();
+    }
+}
