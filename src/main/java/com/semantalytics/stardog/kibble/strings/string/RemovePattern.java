48e24ad34a3ce42d24eed5fc3182cf331bcad519
Zachary Whitley <mail@zacharywhitley.com>
2018-02-02 11:08:47 -0500

    Create RemovePattern.java

---
+++ src/main/java/com/semantalytics/stardog/kibble/strings/string/RemovePattern.java
@@ -0,0 +1,45 @@
+package com.semantalytics.stardog.kibble.strings.string;
+
+import com.complexible.stardog.plan.filter.ExpressionEvaluationException;
+import com.complexible.stardog.plan.filter.ExpressionVisitor;
+import com.complexible.stardog.plan.filter.functions.AbstractFunction;
+import com.complexible.stardog.plan.filter.functions.string.StringFunction;
+import org.apache.commons.lang3.StringUtils;
+import org.openrdf.model.Value;
+
+import static com.complexible.common.rdf.model.Values.*;
+
+public final class RemovePattern extends AbstractFunction implements StringFunction {
+
+    protected RemovePattern() {
+        super(2, StringVocabulary.removePattern.stringValue());
+    }
+
+    private Remove(final RemovePattern removePattern) {
+        super(removePattern);
+    }
+
+    @Override
+    protected Value internalEvaluate(final Value... values) throws ExpressionEvaluationException {
+
+        final String string = assertStringLiteral(values[0]).stringValue();
+        final String pattern = assertStringLiteral(values[1]).stringValue();
+
+        return literal(StringUtils.removePattern(string, pattern));
+    }
+
+    @Override
+    public RemovePattern copy() {
+        return new RemovePattern(this);
+    }
+
+    @Override
+    public void accept(final ExpressionVisitor expressionVisitor) {
+        expressionVisitor.visit(this);
+    }
+
+    @Override
+    public String toString() {
+        return StringVocabulary.removePattern.name();
+    }
+}
