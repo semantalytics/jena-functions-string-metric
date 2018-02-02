59ce2fccd10b2ed1696a8c588d7bb33ea85c8710
Zachary Whitley <mail@zacharywhitley.com>
2018-02-02 16:13:57 -0500

    Update UpperCase.java

--- src/main/java/com/semantalytics/stardog/kibble/strings/string/UpperCase.java
+++ src/main/java/com/semantalytics/stardog/kibble/strings/string/UpperCase.java
@@ -1,1 +1,43 @@
+package com.semantalytics.stardog.kibble.strings.string;
 
+import com.complexible.stardog.plan.filter.ExpressionEvaluationException;
+import com.complexible.stardog.plan.filter.ExpressionVisitor;
+import com.complexible.stardog.plan.filter.functions.AbstractFunction;
+import com.complexible.stardog.plan.filter.functions.UserDefinedFunction;
+import org.openrdf.model.Value;
+
+import static com.complexible.common.rdf.model.Values.literal;
+
+public class UpperCase extends AbstractFunction implements UserDefinedFunction {
+
+    public UpperCase() {
+        super(1, StringVocabulary.upperCase.stringValue());
+    }
+
+    public UpperCase(final UpperCase upperCase) {
+        super(upperCase);
+    }
+
+    @Override
+    protected Value internalEvaluate(final Value... values) throws ExpressionEvaluationException {
+
+        final String string = assertStringLiteral(values[0]).stringValue();
+
+        return literal(string.toUpperCase());
+    }
+
+    @Override
+    public UpperCasee copy() {
+        return new UpperCaseFully(this);
+    }
+
+    @Override
+    public void accept(final ExpressionVisitor expressionVisitor) {
+        expressionVisitor.visit(this);
+    }
+
+    @Override
+    public String toString() {
+        return StringVocabulary.upperCase.name();
+    }
+}
