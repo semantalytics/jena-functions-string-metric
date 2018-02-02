87baa5a9170db7354893e7b1ed80f43a1379a159
Zachary Whitley <mail@zacharywhitley.com>
2018-02-02 11:12:52 -0500

    Create RemoveStartIgnoreCase.java

---
+++ src/main/java/com/semantalytics/stardog/kibble/strings/string/RemoveStartIgnoreCase.java
@@ -0,0 +1,45 @@
+package com.semantalytics.stardog.kibble.strings.string;
+
+import com.complexible.stardog.plan.filter.ExpressionEvaluationException;
+import com.complexible.stardog.plan.filter.ExpressionVisitor;
+import com.complexible.stardog.plan.filter.functions.AbstractFunction;
+import com.complexible.stardog.plan.filter.functions.string.StringFunction;
+import org.apache.commons.lang3.StringUtils;
+import org.openrdf.model.Value;
+
+import static com.complexible.common.rdf.model.Values.*;
+
+public final class RemoveStartIgnoreCase extends AbstractFunction implements StringFunction {
+
+    protected RemoveStartIgnoreCase() {
+        super(2, StringVocabulary.removeStartIgnoreCase.stringValue());
+    }
+
+    private RemoveStartIgnoreCase(final RemoveStartIgnoreCase removeStartIgnoreCase) {
+        super(removeStartIgnoreCase);
+    }
+
+    @Override
+    protected Value internalEvaluate(final Value... values) throws ExpressionEvaluationException {
+
+        final String string = assertStringLiteral(values[0]).stringValue();
+        final String remove = assertStringLiteral(values[1]).stringValue();
+
+        return literal(StringUtils.removeStartIgnoreCase(string, remove));
+    }
+
+    @Override
+    public RemoveStartIgnoreCase copy() {
+        return new RemoveStartIgnoreCase(this);
+    }
+
+    @Override
+    public void accept(final ExpressionVisitor expressionVisitor) {
+        expressionVisitor.visit(this);
+    }
+
+    @Override
+    public String toString() {
+        return StringVocabulary.removeStartIgnoreCase.name();
+    }
+}
