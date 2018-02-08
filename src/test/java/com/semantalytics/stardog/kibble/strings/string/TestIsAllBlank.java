e5052c1f0cb442dfaba50d1337a10da89d7b72e0
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 14:55:31 -0500

    Update and rename IsAllBlankTest.java to TestIsAllBlank.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestIsAllBlank.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestIsAllBlank.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class IsAllBlankTest extends AbstractStardogTest {
+public class TesetIsAllBlank extends AbstractStardogTest {
 
     @Test
     public void testAbbreviateMiddle() {
