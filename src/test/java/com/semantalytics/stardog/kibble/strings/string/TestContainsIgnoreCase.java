77f718827d53c767507203b8ebdd816494cb8a10
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 14:40:19 -0500

    Update and rename ContainsIgnoreCaseTest.java to TestContainsIgnoreCase.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestContainsIgnoreCase.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestContainsIgnoreCase.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class ContainsIgnoreCaseTest extends AbstractStardogTest {
+public class TestContainsIgnoreCase extends AbstractStardogTest {
 
     @Test
     public void testAbbreviateMiddle() {
