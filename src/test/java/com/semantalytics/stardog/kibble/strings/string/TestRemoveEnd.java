5432c4a8590509066df2655fb86a24a02c6ebca4
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 15:45:40 -0500

    Create TestRemoveEnd.java

---
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestRemoveEnd.java
@@ -0,0 +1,130 @@
+package com.semantalytics.stardog.kibble.strings.string;
+
+import com.semantalytics.stardog.kibble.AbstractStardogTest;
+import org.junit.*;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.TupleQueryResult;
+
+import static org.junit.Assert.*;
+
+public class TestRemoveEnd extends AbstractStardogTest {
+
+    @Test
+    public void testAbbreviateMiddle() {
+      
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:removeEnd(\"Stardog graph database\", \"...\", 8) AS ?result) }";
+
+            try (final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final String aValue = aResult.next().getValue("result").stringValue();
+
+                assertEquals("Sta...se", aValue);
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testEmptyString() {
+      
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:removeEnd(\"\", \"\", 10) as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+           
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final String aValue = aResult.next().getValue("result").stringValue();
+
+                assertEquals("", aValue);
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testTooFewArgs() {
+      
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:removeEnd(\"one\") as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+         
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testTooManyArgs() {
+
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:removeEnd(\"one\", 2, \"three\") as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testWrongTypeFirstArg() {
+       
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:removeEnd(4, 5) as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testWrongTypeSecondArg() {
+     
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:removeEnd(\"one\", \"two\") as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testLengthTooShort() {
+     
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:removeEnd(\"Stardog\", 3) as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+         
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+}
