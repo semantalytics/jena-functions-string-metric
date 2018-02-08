fe65d1a89154bbc669ad4fb0a9d0ce3f3944727c
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 15:03:56 -0500

    Update and rename StripAccentsTest.java to TestStripAccents.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestStripAccents.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestStripAccents.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class StripAccentsTest  extends AbstractStardogTest {
+public class TestStripAccents extends AbstractStardogTest {
 
     @Test
     public void testOneArgumentWithoutAccents() {
