512337db67a1d2766081801c7a5fadc247fa0a70
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 15:16:47 -0500

    Update and rename OverlayTest.java to TestOverlay.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestOverlay.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestOverlay.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class OverlayTest  extends AbstractStardogTest {
+public class TestOverlay extends AbstractStardogTest {
 
     @Test
     public void overlayMiddle() {
