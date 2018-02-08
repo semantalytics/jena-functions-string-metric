b1d658eaf0e8d20bf5459f6ff1205616a888bd02
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 14:37:43 -0500

    Update and rename CaseFormatTest.java to TestCaseFormat.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestCaseFormat.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestCaseFormat.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class CaseFormatTest extends AbstractStardogTest {
+public class TestCaseFormat extends AbstractStardogTest {
  
     @Test
     public void testLowerCamelToUpperUnderscoreByExample() {
