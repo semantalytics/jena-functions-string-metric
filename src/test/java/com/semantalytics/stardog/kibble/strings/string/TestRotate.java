2f9fe9fc1057ad3e66b569e89fc85c15be7fefe9
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 15:17:48 -0500

    Update and rename RotateTest.java to TestRotate.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestRotate.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestRotate.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class RotateTest  extends AbstractStardogTest {
+public class TestRotate extends AbstractStardogTest {
 
     @Test
     public void testAbbreviateMiddle() {
