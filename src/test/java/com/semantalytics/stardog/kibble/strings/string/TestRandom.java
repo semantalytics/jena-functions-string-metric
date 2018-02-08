b01bfb1155b98cf8000c1d497b8494943eb23d73
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 15:17:21 -0500

    Update and rename RandomTest.java to TestRandom.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestRandom.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestRandom.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class RandomTest extends AbstractStardogTest {
+public class TestRandom extends AbstractStardogTest {
 
     @Test
     public void testAbbreviateMiddle() {
