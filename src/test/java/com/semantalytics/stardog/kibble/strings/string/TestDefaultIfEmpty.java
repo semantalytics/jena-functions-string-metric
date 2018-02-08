69a518afd6b517cc7b8286bd681a7bcc7f280d73
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 14:44:09 -0500

    Update and rename DefaultIfEmpty.java to TestDefaultIfEmpty.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestDefaultIfEmpty.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestDefaultIfEmpty.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class DefaultIfEmpty extends AbstractStardogTest {
+public class TestDefaultIfEmpty extends AbstractStardogTest {
 
     @Test
     public void testAbbreviateMiddle() {
