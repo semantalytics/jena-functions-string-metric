4b2f22663a3e0211761720b27b1c31b7bb6e3ed4
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 15:23:55 -0500

    Create TestIsAlphaSpace.java

---
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestIsAlphaSpace.java
@@ -0,0 +1,166 @@
+package com.semantalytics.stardog.kibble.strings.string;
+
+import com.semantalytics.stardog.kibble.AbstractStardogTest;
+import org.junit.*;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.TupleQueryResult;
+
+import static org.junit.Assert.*;
+
+public class TestIsAlphaSpace extends AbstractStardogTest {
+
+   
+    @Test
+    public void testAbbreviate() {
+    
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(\"Stardog graph database\", 8) AS ?result) }";
+
+            try (final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final String aValue = aResult.next().getValue("result").stringValue();
+
+                assertEquals("Stard...", aValue);
+
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testAbbreviateWithOffset() {
+  
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(\"Stardog graph database\", 15, 5) AS ?result) }";
+
+            try (final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final String aValue = aResult.next().getValue("result").stringValue();
+
+                assertEquals("...og graph ...", aValue);
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testEmptyString() {
+  
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(\"\", 5) as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final String aValue = aResult.next().getValue("result").stringValue();
+
+                assertEquals("", aValue);
+                assertFalse("Should have no more results", aResult.hasNext());
+            }     
+    }
+
+    @Test
+    public void testTooFewArgs() {
+    
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(\"one\") as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testTooManyArgs() {
+
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(\"one\", 9, \"three\", \"four\") as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+       
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testWrongTypeFirstArg() {
+     
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(4, 5) as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+     
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testWrongTypeSecondArg() {
+
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(\"one\", \"two\") as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+   
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testWrongTypeThirdArg() {
+ 
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(\"one\", 9, \"three\") as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+
+    @Test
+    public void testLengthTooShort() {
+     
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:isAlphaSpace(\"Stardog\", 3) as ?result) }";
+
+            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final BindingSet aBindingSet = aResult.next();
+
+                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+}
