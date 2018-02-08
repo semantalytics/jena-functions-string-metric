464efe23dfdcba302a7dd021d621a6a2f8a89bb7
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 15:16:12 -0500

    Update and rename IsWhitespaceTest.java to TestIsWhitespace.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestIsWhitespace.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestIsWhitespace.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class IsWhitespaceTest  extends AbstractStardogTest {
+public class TestIsWhitespace extends AbstractStardogTest {
 
     @Test
     public void testIsWhitespaceFalse() {
