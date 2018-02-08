e90ff1bb681035ad652597e52a14384234678de9
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 14:55:54 -0500

    Update and rename IsAllLowerCaseTest.java to TestIsAllLowerCase.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestIsAllLowerCase.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestIsAllLowerCase.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class IsAllLowerCaseTest  extends AbstractStardogTest {
+public class TestIsAllLowerCase extends AbstractStardogTest {
 
     @Test
     public void testTrue() {
