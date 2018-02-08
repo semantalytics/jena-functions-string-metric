4025e86e056e2712d0db775c46f3f48d42461a54
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 14:36:20 -0500

    Update and rename AbbreviateWithMarkerTest.java to TestAbbreviateWithMarker.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestAbbreviateWithMarker.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestAbbreviateWithMarker.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class AbbreviateWithMarkerTest  extends AbstractStardogTest {
+public class TestAbbreviateWithMarker extends AbstractStardogTest {
 
   
     @Test
