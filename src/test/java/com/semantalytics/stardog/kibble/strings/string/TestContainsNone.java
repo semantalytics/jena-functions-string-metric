e2bd32ca2d53eaca115eab13287fcea73c0627dc
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 14:40:36 -0500

    Update and rename ContainsNoneTest.java to TestContainsNone.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestContainsNone.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestContainsNone.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class ContainsNoneTest  extends AbstractStardogTest {
+public class TestContainsNone extends AbstractStardogTest {
 
     @Test
     public void testAbbreviateMiddle() {
