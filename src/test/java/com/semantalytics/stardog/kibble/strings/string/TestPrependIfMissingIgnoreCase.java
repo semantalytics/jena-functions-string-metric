268bd9f8851f78a7fc815d28f1d494f4280a18f2
Zachary Whitley <mail@zacharywhitley.com>
2018-02-08 15:17:06 -0500

    Update and rename PrependIfMissingIgnoreCaseTest.java to TestPrependIfMissingIgnoreCase.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestPrependIfMissingIgnoreCase.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestPrependIfMissingIgnoreCase.java
@@ -7,7 +7,7 @@ import org.openrdf.query.TupleQueryResult;
 
 import static org.junit.Assert.*;
 
-public class PrependIfMissingIgnoreCaseTest  extends AbstractStardogTest {
+public class TestPrependIfMissingIgnoreCase extends AbstractStardogTest {
 
     @Test
     public void testAbbreviateMiddle() {
