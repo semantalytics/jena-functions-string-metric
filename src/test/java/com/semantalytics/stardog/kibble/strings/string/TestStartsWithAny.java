ac5776a45a191c224098ea53f39fca75f19afceb
Zachary Whitley <mail@zacharywhitley.com>
2018-02-14 15:41:43 -0500

    Update TestStartsWithAny.java

--- src/test/java/com/semantalytics/stardog/kibble/strings/string/TestStartsWithAny.java
+++ src/test/java/com/semantalytics/stardog/kibble/strings/string/TestStartsWithAny.java
@@ -10,35 +10,53 @@ import static org.junit.Assert.*;
 public class TestStartsWithAny extends AbstractStardogTest {
 
     @Test
-    public void testAbbreviateMiddle() {
+    public void testTrue() {
       
         final String aQuery = StringVocabulary.sparqlPrefix("string") +
-                    "select ?result where { bind(string:startsWithAny(\"Stardog graph database\", \"...\", 8) AS ?result) }";
+                    "select ?result where { bind(string:startsWithAny(\"Stardog\", \"Sta\", \"xyz\") AS ?result) }";
 
             try (final TupleQueryResult aResult = connection.select(aQuery).execute()) {
 
                 assertTrue("Should have a result", aResult.hasNext());
 
-                final String aValue = aResult.next().getValue("result").stringValue();
+                final boolean aValue = Boolean.parseBoolean(aResult.next().getValue("result").stringValue());
 
-                assertEquals("Sta...se", aValue);
+                assertEquals(true, aValue);
                 assertFalse("Should have no more results", aResult.hasNext());
             }
     }
 
     @Test
+    public void testFalse() {
+      
+        final String aQuery = StringVocabulary.sparqlPrefix("string") +
+                    "select ?result where { bind(string:startsWithAny(\"Stardog\", \"dog\", \"xyz\") AS ?result) }";
+
+            try (final TupleQueryResult aResult = connection.select(aQuery).execute()) {
+
+                assertTrue("Should have a result", aResult.hasNext());
+
+                final boolean aValue = Boolean.parseBoolean(aResult.next().getValue("result").stringValue());
+
+                assertEquals(false, aValue);
+                assertFalse("Should have no more results", aResult.hasNext());
+            }
+    }
+    
+    
+    @Test
     public void testEmptyString() {
       
         final String aQuery = StringVocabulary.sparqlPrefix("string") +
-                    "select ?result where { bind(string:startsWithAny(\"\", \"\", 10) as ?result) }";
+                    "select ?result where { bind(string:startsWithAny(\"\", \"\") as ?result) }";
 
             try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
            
                 assertTrue("Should have a result", aResult.hasNext());
 
-                final String aValue = aResult.next().getValue("result").stringValue();
+                final boolean aValue = Boolean.parseBoolean(aResult.next().getValue("result").stringValue());
 
-                assertEquals("", aValue);
+                assertEquals(true, aValue);
                 assertFalse("Should have no more results", aResult.hasNext());
             }
     }
@@ -59,29 +77,12 @@ public class TestStartsWithAny extends AbstractStardogTest {
                 assertFalse("Should have no more results", aResult.hasNext());
             }
     }
-
-    @Test
-    public void testTooManyArgs() {
-
-        final String aQuery = StringVocabulary.sparqlPrefix("string") +
-                    "select ?result where { bind(string:startsWithAny(\"one\", 2, \"three\") as ?result) }";
-
-            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
-
-                assertTrue("Should have a result", aResult.hasNext());
-
-                final BindingSet aBindingSet = aResult.next();
-
-                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
-                assertFalse("Should have no more results", aResult.hasNext());
-            }
-    }
-
+    
     @Test
     public void testWrongTypeFirstArg() {
        
         final String aQuery = StringVocabulary.sparqlPrefix("string") +
-                    "select ?result where { bind(string:startsWithAny(4, 5) as ?result) }";
+                    "select ?result where { bind(string:startsWithAny(1, \"two\") as ?result) }";
 
             try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
 
@@ -98,27 +99,10 @@ public class TestStartsWithAny extends AbstractStardogTest {
     public void testWrongTypeSecondArg() {
      
         final String aQuery = StringVocabulary.sparqlPrefix("string") +
-                    "select ?result where { bind(string:startsWithAny(\"one\", \"two\") as ?result) }";
+                    "select ?result where { bind(string:startsWithAny(\"one\", 2) as ?result) }";
 
             try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
 
-                assertTrue("Should have a result", aResult.hasNext());
-
-                final BindingSet aBindingSet = aResult.next();
-
-                assertTrue("Should have no bindings", aBindingSet.getBindingNames().isEmpty());
-                assertFalse("Should have no more results", aResult.hasNext());
-            }
-    }
-
-    @Test
-    public void testLengthTooShort() {
-     
-        final String aQuery = StringVocabulary.sparqlPrefix("string") +
-                    "select ?result where { bind(string:startsWithAny(\"Stardog\", 3) as ?result) }";
-
-            try(final TupleQueryResult aResult = connection.select(aQuery).execute()) {
-         
                 assertTrue("Should have a result", aResult.hasNext());
 
                 final BindingSet aBindingSet = aResult.next();
